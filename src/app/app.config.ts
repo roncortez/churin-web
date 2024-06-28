97581bd6d43725b5cd0b7041430c16382b01fc35
roncortez <ronny.cortez12@gmail.com>
2024-06-28 16:34:17 -0500

    1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideAnimations(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)), 
+    provideAuth(() => getAuth()), 
+    provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, 
+    provideFirestore(() => getFirestore())]
 };
